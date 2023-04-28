5bc29422163c6e2fb00df395a073d6506f128eab
Dyp1xy <derick.kunz@gmail.com>
2023-04-28 18:02:43 -0300

    java done

--- Leibniz’s Formula/Java.java
+++ Leibniz’s Formula/Java.java
@@ -1,30 +1,29 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.math.RoundingMode;
+import java.math.MathContext;
 
 public class Java {
     public static void main(String args[]){
-        //BigInteger loop = new BigInteger("100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"); 
-        //unhinged test above fuck it we ball
-        final BigInteger loop         = new BigInteger("1000000");
+        BigInteger loop = new BigInteger("10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"); 
+        //unhinged test above, fuck it
+        //final BigInteger loop         = new BigInteger("10000000000");
         BigInteger denominator  = new BigInteger("1");
         final BigDecimal numerator    = new BigDecimal("4");
         BigDecimal pi           = new BigDecimal("0"); 
         BigInteger i            = new BigInteger("0");
         while(loop.compareTo(i) > 0){
             //divideAndRemainder retuns an array, x[1] is the remainder, then we compare te remainter to se if it's even
-            //Rounding wrong, gonna see it when i come back
             BigInteger[] x = i.divideAndRemainder(BigInteger.TWO);
             if(x[1].equals(BigInteger.ZERO)){
-                pi = pi.add((numerator.divide(new BigDecimal(denominator), RoundingMode.UP)));
+                pi = pi.add((numerator.divide(new BigDecimal(denominator), MathContext.DECIMAL128)));
             } else {
-                pi = pi.subtract(numerator.divide(new BigDecimal(denominator), RoundingMode.UP));
+                pi = pi.subtract(numerator.divide(new BigDecimal(denominator), MathContext.DECIMAL128));
             }
             
             //System.out.println(x[0].toString());
             denominator = denominator.add(BigInteger.TWO);
             i = i.add(BigInteger.ONE);
         }
-        System.out.println(pi.toEngineeringString());
+        System.out.println(pi.toString());
     }
 }
