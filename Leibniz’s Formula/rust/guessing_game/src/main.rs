d8d304e862499d1d9e0515a61c5d60052d3f184b
Dyp1xy <derick.kunz@gmail.com>
2023-05-30 14:02:43 -0300

    gonna see it later

--- Leibniz’s Formula/rust/guessing_game/src/main.rs
+++ Leibniz’s Formula/rust/guessing_game/src/main.rs
@@ -1,19 +1,17 @@
-use std::io;
-
-fn generic() {
-    println!("Generic.");
-
-}
-
 fn main() {
-    println!("Guess the number!");
-    let mut guess = String::new(); 
-
-    generic();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {guess}");
+    let numerator = 4.0;
+    let mut denominator = 1.0;
+    let mut pi = 0.0;
+    let iterations = 100000000;
+    
+    for i in 0..iterations+1 {
+        if i % 2 == 0 {
+            pi += numerator/denominator;
+        }
+        else {
+            pi -= numerator/denominator ;
+            denominator += 2.0;
+        }
+    }
+    println!("{pi}");
 }
